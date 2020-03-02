4e7af23b747a6f93fc7b09a09b4981a69eea9e87
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-02 11:12:19 +0100

    added ApplicationDbContext.cs and rectify connectionString

---
+++ WebApplication1/Models/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext():base()
+        {
+
+        }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+
+
+        public virtual DbSet<Candidate> Candidate { get; set; }
+        public virtual DbSet<Voter> Voter { get; set; }
+        public virtual DbSet<Vote> Vote { get; set; }
+        public virtual DbSet<Structure> Structure { get; set; }
+        public virtual DbSet<StructureLevel> StructureLevel { get; set; }
+    }
+}
