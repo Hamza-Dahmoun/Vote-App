d0b1e8e84b2f493550ab6a90a01bb6c46a4a073b
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-02 10:51:08 +0100

    add classes: Person, Structure and StructureLevel

---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public Structure Structure { get; set; }
+    }
+}
---
+++ WebApplication1/Models/StructureLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class StructureLevel
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Structure> Structures { get; set; }
+    }
+}
