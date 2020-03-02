d53788769d058c5f7fd7440845ed87e3e731fb85
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-02 15:39:53 +0100

    add IUnitOfWork interface

---
+++ WebApplication1/Models/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.Repositories;
+
+namespace WebApplication1.Models.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+        int SaveChanges();
+    }
+}
