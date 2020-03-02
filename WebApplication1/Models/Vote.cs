84c547ace1e5b87ccf80298a3e6720296f6e0dd0
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-02 10:54:03 +0100

    added classes: Vote, Voter, Candidate

---
+++ WebApplication1/Models/Vote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Vote
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public DateTime datetime { get; set; }
+        [Required]
+        public Voter Voter { get; set; }
+        [Required]
+        public Candidate Candidate { get; set; }
+
+    }
+}
