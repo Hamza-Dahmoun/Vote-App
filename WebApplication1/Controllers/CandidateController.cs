c7b588cd96818b7e495c9c3cb8041b64cbeebfe1
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-03 13:48:45 +0100

    add Details action to CandidateController, and its view

--- WebApplication1/Controllers/CandidateController.cs
+++ WebApplication1/Controllers/CandidateController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 using WebApplication1.Models.Repositories;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -21,5 +22,44 @@ namespace WebApplication1.Controllers
 
             return View(_candidateRepository.GetAll());
         }
+
+
+        public IActionResult Details(Guid id)
+        {
+            return View(convertCandidate_toPersonViewModel(_candidateRepository.GetById(id)));
+        }
+
+
+
+
+
+
+
+        //******************** UTILITIES
+        public PersonViewModel convertCandidate_toPersonViewModel(Candidate candidate)
+        {
+            PersonViewModel p = new PersonViewModel
+            {
+                Id = candidate.Id,
+                FirstName = candidate.FirstName,
+                LastName = candidate.LastName,
+                StructureName = candidate.Structure?.Name,
+                StructureLevel = candidate.Structure?.Level.Name
+            };
+
+            return p;
+        }
+
+        public List<PersonViewModel> convertCandidateList_toPersonViewModelList(IList<Candidate> candidates)
+        {
+            List<PersonViewModel> myList = new List<PersonViewModel>();
+            foreach (var item in candidates)
+            {
+                myList.Add(convertCandidate_toPersonViewModel(item));
+            }
+
+            return myList;
+        }
+
     }
 }
