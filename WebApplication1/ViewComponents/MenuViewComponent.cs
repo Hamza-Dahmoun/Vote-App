6586c390150ffe87d871cca5e5948c296c0022e1
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-11 13:29:55 +0100

    created MenuViewComponent.cs class inherits from ViewComponent, & with InvokeAsync() method

---
+++ WebApplication1/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.ViewComponents
+{
+    //we made the class inherit from ViewComponent
+    public class MenuViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
