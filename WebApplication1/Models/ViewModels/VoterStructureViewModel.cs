bc28e4a89d50576d88097b8bcce9ae8192d20955
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-03-15 10:24:13 +0100

    add VoterStructureViewModel.cs to be used when adding a new voter

---
+++ WebApplication1/Models/ViewModels/VoterStructureViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class VoterStructureViewModel
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public Structure Structure { get; set; }
+
+        //the below list of structures is used to be displayed when adding a new voter
+        public IList<Structure> Structures { get; set; }
+
+    }
+}
