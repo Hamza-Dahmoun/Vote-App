38552ef15ba9939c1cfb50eddd866379a47c97a2
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-04-13 21:21:46 +0200

    mark the controller for authorization, and inject some services using the constructor (constructor dependency injection)

--- WebApplication1/Controllers/ElectionController.cs
+++ WebApplication1/Controllers/ElectionController.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.Repositories;
 
 namespace WebApplication1.Controllers
 {
+    //the below attribute will permit only authorized users to access HomeController, anonymous access will be deactivated
+    [Authorize]
+    //the below attribute will permit only users with set of roles contained in the policy 'ManageElections'
+    //(you can check the set of roles related to this policy in ConfigureServices() in Startup file)
+    [Authorize(Policy = nameof(VoteAppPolicies.ManageElections))]
+    //[Authorize(Roles = "Administrator")]
     public class ElectionController : Controller
     {
+        //the below are services we're going to use in this controller, they will be injected in the constructor
+        public IRepository<Voter> _voterRepository { get; }
+        public IRepository<Candidate> _candidateRepository { get; }
+        //Lets inject the services using the constructor, this is called Constructor Dependency Injection
+        public ElectionController(IRepository<Voter> voterRepository, IRepository<Candidate> candidateRepository)
+        {
+            _voterRepository = voterRepository;
+            _candidateRepository = candidateRepository;
+        }
+
+
         // GET: Election
         public ActionResult Index()
         {
