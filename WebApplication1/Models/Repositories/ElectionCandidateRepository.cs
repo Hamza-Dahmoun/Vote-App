2fd681c52f65fc518e29c0a956c96db624eed7be
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-04-14 21:04:07 +0200

    write methods of ElectionCandidateRepository.cs implementing IRepository.cs interface

--- WebApplication1/Models/Repositories/ElectionCandidateRepository.cs
+++ WebApplication1/Models/Repositories/ElectionCandidateRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,29 +8,45 @@ namespace WebApplication1.Models.Repositories
 {
     public class ElectionCandidateRepository : IRepository<ElectionCandidate>
     {
+        //Registering the needed services
+        protected readonly VoteDBContext _dbContext;
+        private readonly DbSet<ElectionCandidate> _dbSet;
+
+        //Injecting the services thru the constructor (Constructor Dependency Injection)
+        public ElectionCandidateRepository(VoteDBContext voteDBContext)
+        {//Injecting the services thru the constructor (Constructor Dependency Injection)
+            _dbContext = voteDBContext;
+            _dbSet = _dbContext.Set<ElectionCandidate>();
+        }
+
         public void Add(ElectionCandidate item)
         {
-            throw new NotImplementedException();
+            _dbContext.Add(item);
+            _dbContext.SaveChanges();
         }
 
         public void Delete(Guid Id)
         {
-            throw new NotImplementedException();
+            _dbContext.Remove(GetById(Id));
+            _dbContext.SaveChanges();
         }
 
         public void Edit(Guid Id, ElectionCandidate item)
         {
-            throw new NotImplementedException();
+            var myItem = GetById(Id);
+            myItem.ElectionId = item.ElectionId;
+            myItem.CandidateId = item.CandidateId;
+            _dbContext.SaveChanges();
         }
 
         public IList<ElectionCandidate> GetAll()
         {
-            throw new NotImplementedException();
+            return _dbSet.ToList();
         }
 
         public ElectionCandidate GetById(Guid Id)
         {
-            throw new NotImplementedException();
+            return _dbSet.SingleOrDefault(e=>e.Id == Id);
         }
     }
 }
