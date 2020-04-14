70aed33b047fd28ca2d416b8ab3a3ba2b2fdfd46
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-04-14 21:32:48 +0200

    create ElectionViewModel.cs

---
+++ WebApplication1/Models/ViewModels/ElectionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.ViewModels
+{
+    [NotMapped]
+    public class ElectionViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationInDays { get; set; }
+        public bool HasNeutral { get; set; }
+        public int NumberOfCandidates { get; set; }
+        public int NumberOfVoters { get; set; }
+    }
+}
