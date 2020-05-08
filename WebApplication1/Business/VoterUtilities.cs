78d9a5248a01ed0eed6c347fe8e02c38924abc67
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-08 16:24:57 +0200

    use GetOneFiltered() in getVoterByUserId()

--- WebApplication1/Business/VoterUtilities.cs
+++ WebApplication1/Business/VoterUtilities.cs
@@ -23,16 +23,16 @@ namespace WebApplication1.Business
         public static Voter getVoterByUserId(Guid userId, IRepository<Voter> voterRepository)
         {//this is using Method Dependancy Injection
             _voterRepository = voterRepository;
-            return _voterRepository.GetAll().SingleOrDefault(v => v.UserId == userId);
+
+
+            //declaring an expression that is special to Voter objects
+            System.Linq.Expressions.Expression<Func<Voter, bool>> expr = v => v.UserId == userId;
+
+
+            return _voterRepository.GetOneFiltered(expr);
+        
         }
 
-        /*
-        //Note that this method uses _voterRepository, so it depends to it, and we passed the repository object as a pramater. This is called Method Dependancy Injection
-        public static int getNumberOfVoterWithVote(IRepository<Voter> voterRepository)
-        {//this is using Method Dependancy Injection
-            _voterRepository = voterRepository;
-            return _voterRepository.GetAll().Where(v => v.hasVoted() == true).Count();
-        }*/
 
 
         //Note that this method uses _voterRepository, so it depends to it, and we passed the repository object as a pramater. This is called Method Dependancy Injection
