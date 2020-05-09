34a3525c51c7eaf9fb3b6425c701ef08e5e9b978
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-09 17:21:49 +0200

    add VoterController/DataTable() to get voters for jQuery datatables

--- WebApplication1/Controllers/VoterController.cs
+++ WebApplication1/Controllers/VoterController.cs
@@ -169,8 +169,148 @@ namespace WebApplication1.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        
 
-        
+
+
+
+        #region JQUERY DATATABLE REGION
+        public IActionResult DataTable()
+        {
+            //This method is called by jQuery datatables to get paged data
+            //First, we'll try to read the variables sent from the jQuery request, and then, based on these variables' values we'll query
+            //the db
+
+            
+            try
+            {
+                //lets first get the variables of the request (of the form), and then build the linq query accordingly
+                //above each variable I wrote the official doc of jQuery
+
+                /*
+                 * draw
+                 * integer Type
+                 Draw counter. This is used by DataTables to ensure that the Ajax returns from server-side processing requests 
+                 are drawn in sequence by DataTables (Ajax requests are asynchronous and thus can return out of sequence). 
+                 This is used as part of the draw return parameter (see below).
+                 */
+                var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+
+
+                /*
+                 * start
+                 * integer type
+                 Paging first record indicator. This is the start point in the current data set (0 index based - i.e. 0 is the first record).
+                 */
+                var start = HttpContext.Request.Form["start"].FirstOrDefault();
+
+
+                /*
+                 * length
+                 * integer type
+                 Number of records that the table can display in the current draw. It is expected that the number of records returned 
+                 will be equal to this number, unless the server has fewer records to return. Note that this can be -1 to indicate that 
+                 all records should be returned (although that negates any benefits of server-side processing!)
+                 */
+                var length = HttpContext.Request.Form["length"].FirstOrDefault();
+
+
+                /*
+                 * search[value]
+                 * string Type
+                 Global search value. To be applied to all columns which have searchable as true.
+                 */
+                var searchValue = HttpContext.Request.Form["search[value]"].FirstOrDefault();
+
+                /*
+                 * order[i][column]
+                 * integer Type
+                 Column to which ordering should be applied. This is an index reference to the columns array of information 
+                 that is also submitted to the server.
+                 */
+                 //for now we'll consider sorting voters only by their FirstName (second column) later we'll see
+                var sortColumnName = HttpContext.Request.Form["order[1][column][name]"].FirstOrDefault();
+                //var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+                /*
+                 * order[i][dir]
+                 * integer Type
+                 Ordering direction for this column. It will be asc or desc to indicate ascending ordering or descending ordering, respectively.
+                 */
+                var sortColumnDirection = HttpContext.Request.Form["order[0][dir]"].FirstOrDefault();
+
+
+                //Page Size (10, 20, 50,100) 
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+
+                //how many rows too skip?
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+
+                //totalRecords too inform user
+                int totalRecords = 0;
+
+                //lets get all voters (I know I know it is not good, but this is only for testing, I'll write a new method in repositories 
+                //and use it to get data filtered and paged later)
+                var voters = _voterRepository.GetAll();
+
+                //lets sort voters if user asked to
+                if (!string.IsNullOrEmpty(sortColumnName) && !string.IsNullOrEmpty(sortColumnDirection))
+                {
+                    //I don't know the name of the property I am going to sort with, so I'll use Refection API
+                    //to get the name of the property then I'll use it to order the list
+
+                    System.Reflection.PropertyInfo propertyName = typeof(Voter).GetProperty(sortColumnName);
+                    if(sortColumnDirection == "asc")
+                    {
+                        voters = voters.OrderBy(e => propertyName.GetValue(e)).ToList();
+                    }
+                    if (sortColumnDirection == "disc")
+                    {
+                        voters = voters.OrderByDescending(e => propertyName.GetValue(e)).ToList();
+                    }                    
+                }
+
+                //now lets look for a value in FirstName/LastName/StateName if user asked to
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    voters = voters.Where(
+                        v => v.FirstName.Contains(searchValue) || 
+                        v.LastName.Contains(searchValue) || 
+                        v.State.Name.Contains(searchValue))                        
+                        .ToList();
+                }
+
+                //lets assign totalRecords the correct value
+                totalRecords = voters.Count;
+
+                //lets proceed paging and taking only requested rows
+                var myData = voters.Skip(skip).Take(pageSize)
+                    .Select(v => new
+                    {
+                        v.Id,
+                        v.FirstName,
+                        v.LastName,
+                        State = v.State.Name
+                    }).ToList();
+
+                //now lets return json data so that it is understandable by jQuery
+                return Json(
+                    new
+                    {
+                        draw = draw,
+                        recordsFiltered = totalRecords,
+                        recordsTotal = totalRecords, 
+                        data = myData
+                    }
+                    );
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+        #endregion
+
+
     }
 }
