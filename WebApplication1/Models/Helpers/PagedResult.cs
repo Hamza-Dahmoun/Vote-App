2ff1ef3260589b70c0bc03411f49990c5f2abc30
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-11 01:28:02 +0200

    add Models/Helpers/PagedResult.cs to b used by repositories to respond to jquery datatables

---
+++ WebApplication1/Models/Helpers/PagedResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Helpers
+{
+    public class PagedResult<T>
+    {
+        //this class is used in repositories when responding to jquery datatables
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+
+        public PagedResult(List<T> items, int totalCount)
+        {
+            Items = items;
+            TotalCount = totalCount;
+        }
+    }
+}
