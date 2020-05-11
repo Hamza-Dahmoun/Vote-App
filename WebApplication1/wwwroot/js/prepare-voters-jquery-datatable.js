5689583c8ca9008af868cac09dd412fdb911a0f2
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-11 17:03:25 +0200

    move preparejQueryDatatable() to  a separated file & load it using partialView

---
+++ WebApplication1/wwwroot/js/prepare-voters-jquery-datatable.js
@@ -0,0 +1,21 @@
+﻿//now lets make it a jquery datatables server side processing
+//document.addEventListener("DOMContentLoaded", initializeDatatable);
+document.addEventListener("DOMContentLoaded", preparejQueryDatatable());
+
+
+
+function preparejQueryDatatable() {
+    //this function passes some parameters to initializeDatatable() to get jQueryDatatables running
+    let tableSelector = "#voters-table";
+    let url = '/Voter/DataTable';
+    let columnsArray =
+        [//These are the columns to be displayed, and they are the fields of the voters objects brought from the server
+            { "data": "Id", "visible": false, "searchable": false },
+            { "data": "FirstName", "title": "FirstName", "name": "FirstName", "visible": true, "searchable": true, "sortable": false },
+            { "data": "LastName", "title": "Last Name", "name": "LastName", "visible": true, "searchable": true, "sortable": false },
+            { "data": "State.Name", "title": "State", "visible": true, "searchable": true, "sortable": false }
+        ]
+        ;
+    initializeDatatable(tableSelector, url, columnsArray);
+}
+
