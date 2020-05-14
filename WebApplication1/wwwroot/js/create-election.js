8b18a32441dd4954221600e7c47d9ded7519ef73
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-14 16:27:55 +0200

    removed html elmts and js code of the old way (before datatables)

--- WebApplication1/wwwroot/js/create-election.js
+++ WebApplication1/wwwroot/js/create-election.js
@@ -1,11 +1,14 @@
 ﻿/**
  *
- * SCENARIO: A USER ENTERS ALL DATA OF AN ELECION AND SUBMIT. IF IT WAS SUCCESSFULLY INSERTED WE'LL GET A RESPONSE FROM SERVER CONTAINING THE
- * ELECTION ITSELF AND A LIST OF VOTERS.
- * THE LIST OF VOTERS WILL BE DISPLAYED FOR THE USER TO SELECT WHICH ONES ARE THE CANDIDATES.
- * WHEN A USER SELECT A CANDIDATE, WE'LL USE JQUERY TO SEND THE ID OF THE ELECTION ALONG WITH THE ID OF THE VOTER SELECTED AS A CANDIDATE
- * TO THE SERVER
- *
+ * SCENARIO: 
+ * A USER ENTERS ALL DATA OF AN ELECION AND SUBMIT. IF IT WAS SUCCESSFULLY INSERTED WE'LL GET A RESPONSE FROM SERVER CONTAINING THE
+ * ELECTION ITSELF, AND INVOKE JQUERY DATATABLE SERVER SIDE PROCESSING TO GET THE LIST OF VOTERS WITH A SELECT CANDIDATE BUTTON.
+ * USER SELECT WHICH ONES ARE THE CANDIDATES. ON EACH CLICK A JQUERY AJAX REQUEST IS SENT TO THE SERVER TO CREATE A NEW CANDIDATE FOR THIS ELECTION.
+ * IF THE CANDIDATE WAS CREATED SUCCESSFULLY, WE'LL DISPLAY IT IN A CANDIDATES AREA FOR THE USER TO SEE WHAT HE ALREADY SELECTED ASS CANDIDATE.
+ * THE CANDIDATES ARE DUSPLAYED WITH A REMOVE BUTTON IN CASE USER WANTED TO REMOVE A CANDIDATE FROM THE ELECTION, IT ALSO RELEASE A JQUERY AJAX CALL
+ * TO THE SERVER TO REMOVE THE CONCERNED CANDIDATE.
+ * WHEN DONE USER GOES BACK TO THE HOME PAGE
+ * 
  * */
 //this variable is gonna be assigned the new election Id in server side
 var electionId;
@@ -58,7 +61,7 @@ function sendElection() {
             electionId = response.ElectionId;
             disableAllInputs();
             changeBgColor();
-            displayVoters(response.Voters);
+
             prepareVotersjQueryDatatable(response.ElectionId);
             scrollDown();
             //window.location.href = "Home/Index";
@@ -87,139 +90,13 @@ function scrollDown() {
     //this function scroll down to step2
     document.getElementById("step-two").scrollIntoView();
 }
-function displayVoters(voters) {
-    //console.log(voters);
-    var tableBody = document.getElementById("old-voters-table").querySelector("tbody");
-    for (let i = 0; i < voters.length; i++) {
-        let tdFirstName = document.createElement("td");
-        tdFirstName.innerText = voters[i].FirstName;
-        let tdLastName = document.createElement("td");
-        tdLastName.innerText = voters[i].LastName;
-        let tdState = document.createElement("td");
-        tdState.innerText = voters[i].StateName;
-
-        let tdButton = document.createElement("td");
-        let addCandidateButton = document.createElement("a");
-        addCandidateButton.innerHTML = "Select as Candidate";
-        addCandidateButton.style.cursor = "pointer";
-        addCandidateButton.style.color = "#3262ec";
-        addCandidateButton.setAttribute("voterid", voters[i].Id);
-        addCandidateButton.addEventListener("click", sendCandidate);
-        tdButton.appendChild(addCandidateButton);
-
-        let removeCandidateButton = document.createElement("a");
-        removeCandidateButton.innerHTML = "Remove Candidate";
-        removeCandidateButton.style.cursor = "pointer";
-        removeCandidateButton.style.color = "red";
-        removeCandidateButton.style.display = "none";
-        removeCandidateButton.setAttribute("voterid", voters[i].Id);
-        removeCandidateButton.addEventListener("click", removeCandidate);
-        tdButton.appendChild(removeCandidateButton);
-
-        //now lets add a spinner to be shown when one of these buttons is clicked
-        let spinner = document.createElement("span");
-        spinner.className = "spinner-border";
-        spinner.style.display = "none";
-        tdButton.appendChild(spinner);
-
-        let voterRow = document.createElement("tr");
-        voterRow.appendChild(tdFirstName);
-        voterRow.appendChild(tdLastName);
-        voterRow.appendChild(tdState);
-        voterRow.appendChild(tdButton);
-
-        tableBody.appendChild(voterRow);
-    }
-    document.getElementById("step-two").style.display = "block";
-}
-function sendCandidate(event) {
-    //first of all lets display the spinner and hide the button
-    displayBrotherSpinner(event.target);
-    event.target.style.display = "none";
-
-    let voterid = event.target.getAttribute("voterid");
-
-    //Send the JSON data of voterId and electionId to Controller using AJAX.
-    $.ajax({
-        type: "POST",
-        url: "/Election/AddCandidates",
-        data: JSON.stringify({ electionId: electionId, voterId: voterid }),//JSON.stringify(newElection),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        error: function () {
-            //alert("error");
-
-        },
-        success: function (response) {
-            //'response' represents the object returned from the api which is the Election object newly stored in the db
-            //console.log(response);
-            //alert("success" + response);
-            switchCandidateButton(event.target);
-
-            //window.location.href = "Home/Index";
-        }
-    });
-}
-function displayBrotherSpinner(btn) {
-    let spinner = btn.parentElement.querySelectorAll(".spinner-border")[0];
-    spinner.style.display = "block";
-}
-function hideBrotherSpinner(btn) {
-    let spinner = btn.parentElement.querySelectorAll(".spinner-border")[0];
-    spinner.style.display = "none";
-}
-function switchCandidateButton(element) {
-    //first of all lets hide the spinner
-    hideBrotherSpinner(element);
-
-    element.style.display = "none";
-
-    let buttons = element.parentElement.querySelectorAll("a");
-    for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i] != element) {
-            //so lets display the other button
-            buttons[i].style.display = "block";
-        }
-    }
-}
-
-function removeCandidate(event) {
-    //this function remove the relation between election and candidates
-
-    //first of all lets display the spinner and hide the button
-    displayBrotherSpinner(event.target);
-    event.target.style.display = "none";
-
-    let voterid = event.target.getAttribute("voterid");
-
-    //Send the JSON data of voterId and electionId to Controller using AJAX.
-    $.ajax({
-        type: "POST",
-        url: "/Election/RemoveCandidate",
-        data: JSON.stringify({ electionId: electionId, voterId: voterid }),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        error: function () {
-            //alert("error");
-        },
-        success: function (response) {
-            //console.log(response);
-            //alert("success" + response);
-            switchCandidateButton(event.target);
-
-            //window.location.href = "Home/Index";
-        }
-    });
-}
-
-
 
 
 
 //********************************************* AFTER USING JQUERY DATATABLES *********************/
 function prepareVotersjQueryDatatable(electionId) {
-    //console.log("-" + electionId + "-");
-
+    console.log("-" + electionId + "-");
+    document.getElementById("step-two").style.display = "block";
     //this function send a request to the server to get the list of voters not candidates to a fiven election
     $("#voters-table").DataTable(
         {
