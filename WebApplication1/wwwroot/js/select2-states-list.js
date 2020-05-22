f4d4f8a16aea5b7d974cd0255e3c32c2073069b6
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-05-22 03:26:43 +0200

    put select2 states list js code into a separate file & mention its path in a new partialView

---
+++ WebApplication1/wwwroot/js/select2-states-list.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    $("#stateId").select2({
+        ajax: {
+            type: "POST",
+            url: "/Voter/States",
+            dataType: 'json',
+            delay: 250,//wait for 250ms after user stopped tying, then send the request to the server
+            data: function (params) {
+                return {
+                    q: params.term, // search term
+                    page: params.page
+                };
+            },
+            processResults:
+                function (data) {
+                    //data is an object we've got from server and has one property 'states' which is an array of {id: .., text: ..}
+                    //console.log(data.states);                        
+                    return {
+                        results: data.states
+                    };
+                },
+
+        },
+        minimumInputLength: 1, // Minimum length of input in search box before ajax call triggers        
+
+    });
+})
