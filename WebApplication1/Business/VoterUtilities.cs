4ee5dff4a9a5ab7fceb3cf03fd0ca41f482a37b2
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-06-14 16:56:01 +0200

    throw possible exceptions in voterUtilities.cs

--- WebApplication1/Business/VoterUtilities.cs
+++ WebApplication1/Business/VoterUtilities.cs
@@ -21,16 +21,23 @@ namespace WebApplication1.Business
 
         //Note that this method uses _voterRepository, so it depends to it, and we passed the repository object as a pramater. This is called Method Dependancy Injection
         public static Voter getVoterByUserId(Guid userId, IRepository<Voter> voterRepository)
-        {//this is using Method Dependancy Injection
-            _voterRepository = voterRepository;
+        {
+            try
+            {
+                //this is using Method Dependancy Injection
+                _voterRepository = voterRepository;
 
 
-            //declaring an expression that is special to Voter objects
-            System.Linq.Expressions.Expression<Func<Voter, bool>> expr = v => v.UserId == userId;
+                //declaring an expression that is special to Voter objects
+                System.Linq.Expressions.Expression<Func<Voter, bool>> expr = v => v.UserId == userId;
 
 
-            return _voterRepository.GetOneFiltered(expr);
-        
+                return _voterRepository.GetOneFiltered(expr);
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
 
@@ -39,10 +46,17 @@ namespace WebApplication1.Business
         public static List<Voter> getOtherVoters(IRepository<Voter> voterRepository, List<Voter> voters)
         {//this is using Method Dependancy Injection
 
-            //this method takes a list of Voters and return a list of voters different than those mentioned previously
+            try
+            {
+                //this method takes a list of Voters and return a list of voters different than those mentioned previously
 
-            _voterRepository = voterRepository;
-            return _voterRepository.GetAll().Except(voters).ToList();
+                _voterRepository = voterRepository;
+                return _voterRepository.GetAll().Except(voters).ToList();
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
 
@@ -51,11 +65,18 @@ namespace WebApplication1.Business
         public static string getStateName(IRepository<Voter> voterRepository, Guid voterId)
         {//this is using Method Dependancy Injection
 
-            //this method returns the state name of a voter, I found myself obliged to write this bcuz I am using eager loading
-            //and when I wanted to access a candidate's state name thru candidate.voterbeing.state.name it was always null
+            try
+            {
+                //this method returns the state name of a voter, I found myself obliged to write this bcuz I am using eager loading
+                //and when I wanted to access a candidate's state name thru candidate.voterbeing.state.name it was always null
 
-            _voterRepository = voterRepository;
-            return _voterRepository.GetById(voterId).State.Name;
+                _voterRepository = voterRepository;
+                return _voterRepository.GetById(voterId).State.Name;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }
         }
 
 
