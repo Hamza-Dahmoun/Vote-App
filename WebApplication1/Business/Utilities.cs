7e2bcfe59db40e072b139c6052b631d9a1f46608
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-06-14 16:51:04 +0200

    throw possible exceptions in Utilities.cs

--- WebApplication1/Business/Utilities.cs
+++ WebApplication1/Business/Utilities.cs
@@ -64,30 +64,44 @@ namespace WebApplication1.Business
 
         public static PersonViewModel convertVoter_toPersonViewModel(Voter voter)
         {
-            PersonViewModel p = new PersonViewModel
+            try
             {
-                Id = voter.Id,
-                FirstName = voter.FirstName,
-                LastName = voter.LastName,
-                StateName = voter.State?.Name
-            };
-            /*if (voter.hasVoted())
-                p.hasVoted = "Yes";
-            else p.hasVoted = "No";
-            */
+                PersonViewModel p = new PersonViewModel
+                {
+                    Id = voter.Id,
+                    FirstName = voter.FirstName,
+                    LastName = voter.LastName,
+                    StateName = voter.State?.Name
+                };
+                /*if (voter.hasVoted())
+                    p.hasVoted = "Yes";
+                else p.hasVoted = "No";
+                */
 
-            return p;
+                return p;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
         public static List<PersonViewModel> convertVoterList_toPersonViewModelList(IList<Voter> voters)
         {
-            List<PersonViewModel> myList = new List<PersonViewModel>();
-            foreach (var item in voters)
+            try
             {
-                myList.Add(convertVoter_toPersonViewModel(item));
-            }
+                List<PersonViewModel> myList = new List<PersonViewModel>();
+                foreach (var item in voters)
+                {
+                    myList.Add(convertVoter_toPersonViewModel(item));
+                }
 
-            return myList;
+                return myList;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
 
@@ -95,22 +109,36 @@ namespace WebApplication1.Business
 
         public static StateViewModel convertState_toStateViewModel(State state)
         {
-            StateViewModel s = new StateViewModel
+            try
             {
-                Id = state.Id,
-                Name = state.Name
-            };
-            return s;
+                StateViewModel s = new StateViewModel
+                {
+                    Id = state.Id,
+                    Name = state.Name
+                };
+                return s;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
         public static List<StateViewModel> convertStateList_toStateViewModelList(IList<State> states)
         {
-            List<StateViewModel> myList = new List<StateViewModel>();
-            foreach (var item in states)
+            try
             {
-                myList.Add(convertState_toStateViewModel(item));
+                List<StateViewModel> myList = new List<StateViewModel>();
+                foreach (var item in states)
+                {
+                    myList.Add(convertState_toStateViewModel(item));
+                }
+                return myList;
             }
-            return myList;
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
 
@@ -119,31 +147,45 @@ namespace WebApplication1.Business
 
         public static ElectionViewModel convertElection_toElectionViewModel(Election election)
         {
-            //int numberOfCandidates  = election.Candidates.Count();
-            ElectionViewModel e = new ElectionViewModel
+            try
             {
-                Id = election.Id,
-                Name = election.Name,
-                StartDate = election.StartDate,
-                DurationInDays = election.DurationInDays,
-                HasNeutral = election.HasNeutral,
-                NumberOfCandidates = election.Candidates.Where(c =>c.isNeutralOpinion != true).Count(),
-                NumberOfVotes = election.Votes.Count()
-                //NumberOfVoters = election.ElectionVoters.Count()
-            };
-            
-            return e;
+                //int numberOfCandidates  = election.Candidates.Count();
+                ElectionViewModel e = new ElectionViewModel
+                {
+                    Id = election.Id,
+                    Name = election.Name,
+                    StartDate = election.StartDate,
+                    DurationInDays = election.DurationInDays,
+                    HasNeutral = election.HasNeutral,
+                    NumberOfCandidates = election.Candidates.Where(c => c.isNeutralOpinion != true).Count(),
+                    NumberOfVotes = election.Votes.Count()
+                    //NumberOfVoters = election.ElectionVoters.Count()
+                };
+
+                return e;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
         public static List<ElectionViewModel> convertElectionList_toElectionViewModelList(IList<Election> elections)
         {
-            List<ElectionViewModel> myList = new List<ElectionViewModel>();
-            foreach (var item in elections)
+            try
             {
-                myList.Add(convertElection_toElectionViewModel(item));
-            }
+                List<ElectionViewModel> myList = new List<ElectionViewModel>();
+                foreach (var item in elections)
+                {
+                    myList.Add(convertElection_toElectionViewModel(item));
+                }
 
-            return myList.OrderBy(c => c.StartDate).ToList();
+                return myList.OrderBy(c => c.StartDate).ToList();
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
 
 
@@ -154,34 +196,55 @@ namespace WebApplication1.Business
 
         public static VoterCandidateEntityViewModel convertVoter_toVoterCandidateEntityViewModel(Voter v)
         {
-            VoterCandidateEntityViewModel vc = new VoterCandidateEntityViewModel();
-            vc.VoterId = v.Id.ToString();
-            vc.FirstName = v.FirstName;
-            vc.LastName = v.LastName;
-            vc.StateName = v.State.Name;
-            return vc;
+            try
+            {
+                VoterCandidateEntityViewModel vc = new VoterCandidateEntityViewModel();
+                vc.VoterId = v.Id.ToString();
+                vc.FirstName = v.FirstName;
+                vc.LastName = v.LastName;
+                vc.StateName = v.State.Name;
+                return vc;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
         public static VoterCandidateEntityViewModel convertCandidate_toVoterCandidateEntityViewModel(IRepository<Voter> voterRepository, Candidate c)
         {
-            VoterCandidateEntityViewModel vc = new VoterCandidateEntityViewModel();
-            vc.VoterId = c.VoterBeing.Id.ToString();
-            vc.FirstName = c.VoterBeing.FirstName;
-            vc.LastName = c.VoterBeing.LastName;
-            vc.StateName = VoterUtilities.getStateName(voterRepository, c.VoterBeing.Id);
-            vc.CandidateId = c.Id.ToString();
-            return vc;
+            try
+            {
+                VoterCandidateEntityViewModel vc = new VoterCandidateEntityViewModel();
+                vc.VoterId = c.VoterBeing.Id.ToString();
+                vc.FirstName = c.VoterBeing.FirstName;
+                vc.LastName = c.VoterBeing.LastName;
+                vc.StateName = VoterUtilities.getStateName(voterRepository, c.VoterBeing.Id);
+                vc.CandidateId = c.Id.ToString();
+                return vc;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
         //this is used and called when editing an election
         public static List<VoterCandidateEntityViewModel> convertVoterList_toVoterCandidateEntityViewModelList(
             List<VoterCandidateEntityViewModel> myList, 
             List<Voter> voterList)
         {
-            foreach (var item in voterList)
+            try
             {
-                myList.Add(convertVoter_toVoterCandidateEntityViewModel(item));
-            }
+                foreach (var item in voterList)
+                {
+                    myList.Add(convertVoter_toVoterCandidateEntityViewModel(item));
+                }
 
-            return myList;
+                return myList;
+            }
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
         //this is used and called when editing an election
         public static List<VoterCandidateEntityViewModel> convertCandidateList_toVoterCandidateEntityViewModelList(
@@ -189,16 +252,20 @@ namespace WebApplication1.Business
             List<VoterCandidateEntityViewModel> myList,
             List<Candidate> candidateList)
         {//the parameter voterRepository is passed to be used in a Method Dependancy Injection in VoterUtilities.getStateName() method
-            foreach (var item in candidateList)
+            try
             {
-                myList.Add(convertCandidate_toVoterCandidateEntityViewModel(voterRepository, item));
+                foreach (var item in candidateList)
+                {
+                    myList.Add(convertCandidate_toVoterCandidateEntityViewModel(voterRepository, item));
+                }
+
+                return myList;
             }
-
-            return myList;
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
-
-
-
 
 
 
@@ -210,12 +277,19 @@ namespace WebApplication1.Business
 
         public static List<Voter> getCorrespondingVoters(List<Candidate> candidates)
         {
-            List<Voter> voters = new List<Voter>();
-            foreach (var candidate in candidates)
+            try
             {
-                voters.Add(candidate.VoterBeing);
+                List<Voter> voters = new List<Voter>();
+                foreach (var candidate in candidates)
+                {
+                    voters.Add(candidate.VoterBeing);
+                }
+                return voters;
             }
-            return voters;
+            catch(Exception E)
+            {
+                throw E;
+            }            
         }
     }
 }
