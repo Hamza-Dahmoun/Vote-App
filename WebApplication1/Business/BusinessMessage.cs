bcc311f69dbe2e5e0057ac65168ca2fa46250935
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-06-27 23:08:09 +0200

    add BusinessMessage.cs to be used when BusinessException is thrown

---
+++ WebApplication1/Business/BusinessMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Business
+{
+    public class BusinessMessage
+    {
+        public string Type { get; set; }
+        public string Text { get; set; }
+        public BusinessMessage(string type, string text)
+        {
+            Type = type;
+            Text = text;
+        }
+    }
+}
