0f4a1873a4371834d4ca56dbfdfc48c8f61dddf8
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-06-28 17:28:00 +0200

    sued BusinessException & BusinessMessage in Details() action & view

--- WebApplication1/Controllers/StateController.cs
+++ WebApplication1/Controllers/StateController.cs
@@ -59,14 +59,37 @@ namespace WebApplication1.Controllers
             _logger.LogInformation("State/Details() action is called");
             try
             {
+                if (id == null)
+                {
+                    _logger.LogError("Passed parameter 'id' is null");
+                    throw new BusinessException("Passed parameter 'id' can not be null");
+                }
+
                 _logger.LogInformation("Calling StateRepository.GetById() method");
                 State s = _stateRepository.GetById(id);
+
+                if (s == null)
+                {
+                    _logger.LogError("State not found");
+                    throw new BusinessException("State not found");
+                }
+
                 _logger.LogInformation("Calling Utilities.convertState_toStateViewModel() method");
                 StateViewModel svm = Utilities.convertState_toStateViewModel(s);
+                
                 _logger.LogInformation("Returning a StateViewModel to the Details view");
                 return View(svm);
             }
-            catch(Exception E)
+            catch (BusinessException be)
+            {
+                _logger.LogError(be.Message);
+                //lets now create a suitable message for the user and store it inside a ViewBag (which is a Dynamic Object we can fill it
+                //by whatever we want
+                BusinessMessage bm = new BusinessMessage("Error", be.Message);
+                ViewBag.BusinessMessage = bm;
+                return View();
+            }
+            catch (Exception E)
             {
                 _logger.LogError("Exception, " + E.Message);
                 throw E;
@@ -188,7 +211,7 @@ namespace WebApplication1.Controllers
             catch (Exception E)
             {
                 _logger.LogError("Exception, " + E.Message);
-                return BadRequest(E.Message);
+                throw E;
             }         
         }
 
