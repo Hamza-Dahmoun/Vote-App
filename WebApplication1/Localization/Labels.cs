991ae4dab91490d363b36c350fe02361fe07223f
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-07-09 16:04:56 +0200

    rectify the namespace of the localization classes so that the compiler compose a correct Search Location for resources files

--- WebApplication1/Localization/Labels.cs
+++ WebApplication1/Localization/Labels.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Localization
+//namespace WebApplication1.Localization
+//the above namespace will push the compiler to look for the resources files in the location: WebApplication1.Resources.Localization
+//which doesn't exist, so we need to rectify the namespace of Labels.Cs/Messages.cs/Menu.cs by removing the word 'Localization' from it
+
+namespace WebApplication1
 {
     public class Labels
     {
