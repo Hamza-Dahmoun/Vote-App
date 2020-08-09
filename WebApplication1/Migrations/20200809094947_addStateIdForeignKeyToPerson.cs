3628c50fbf1d86e0391cded01b2e8cdead017cf0
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-08-09 10:51:44 +0100

    fix property name StateId instead of StatusId

---
+++ WebApplication1/Migrations/20200809094947_addStateIdForeignKeyToPerson.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class addStateIdForeignKeyToPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voter_State_StateId",
+                table: "Voter");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Voter");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StateId",
+                table: "Voter",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voter_State_StateId",
+                table: "Voter",
+                column: "StateId",
+                principalTable: "State",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voter_State_StateId",
+                table: "Voter");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StateId",
+                table: "Voter",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StatusId",
+                table: "Voter",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voter_State_StateId",
+                table: "Voter",
+                column: "StateId",
+                principalTable: "State",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
