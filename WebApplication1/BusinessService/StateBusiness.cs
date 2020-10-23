d20ca0f34a429dd1cc68b2fd006e44329e1f8a76
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-23 22:16:37 +0100

    add stateBusiness.cs service and register/inject services it needs

---
+++ WebApplication1/BusinessService/StateBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.Repositories;
+
+namespace WebApplication1.BusinessService
+{
+    public class StateBusiness
+    {
+        //the below are services we're going to use in this file, they will be injected in the constructor
+        private readonly IRepository<State> _stateRepository;
+
+        public StateBusiness(IRepository<State> stateRepository)
+        {
+            _stateRepository = stateRepository;
+        }
+    }
+}
