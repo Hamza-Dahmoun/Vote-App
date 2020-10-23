6d6b26ab08f458d3905e14ed18f0563dc193b98e
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-23 22:52:18 +0100

    add VoteBusiness.Cs & register needed services inside it

---
+++ WebApplication1/BusinessService/VoteBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.Repositories;
+
+namespace WebApplication1.BusinessService
+{
+    public class VoteBusiness
+    {
+        //the below are services we're going to use in this file, they will be injected in the constructor
+        private readonly IRepository<Vote> _voteRepository;
+
+        public VoteBusiness(IRepository<Vote> voteRepository)
+        {
+            _voteRepository = voteRepository;
+        }
+    }
+}
