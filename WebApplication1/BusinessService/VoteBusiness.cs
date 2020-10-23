98428dfe4f15e68411cac8a29893c479f17e5d5f
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-23 23:03:56 +0100

    register/inject required services

--- WebApplication1/BusinessService/VoteBusiness.cs
+++ WebApplication1/BusinessService/VoteBusiness.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Business;
 using WebApplication1.Models;
 using WebApplication1.Models.Repositories;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.BusinessService
 {
@@ -11,10 +18,33 @@ namespace WebApplication1.BusinessService
     {
         //the below are services we're going to use in this file, they will be injected in the constructor
         private readonly IRepository<Vote> _voteRepository;
+        //this is used to get the currentUser
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _contextAccessor;
+        //Lets create a private readonly field IStringLocalizer<Messages> so that we can use Localization service, we'll inject it inside the constructor
+        private readonly IStringLocalizer<Messages> _messagesLoclizer;
+        private readonly IRepository<Candidate> _candidateRepository;
+        private readonly IRepository<Voter> _voterRepository;
+        private readonly IRepository<Election> _electionRepository;
 
-        public VoteBusiness(IRepository<Vote> voteRepository)
+        public VoteBusiness(IRepository<Vote> voteRepository,
+            UserManager<IdentityUser> userManager,
+            IHttpContextAccessor contextAccessor,
+            IStringLocalizer<Messages> messagesLoclizer,
+            IRepository<Candidate> candidateRepository,
+            IRepository<Voter> voterRepository,
+            IRepository<Election> electionRepository)
         {
             _voteRepository = voteRepository;
+            _userManager = userManager;
+            _contextAccessor = contextAccessor;
+            _messagesLoclizer = messagesLoclizer;
+            _candidateRepository = candidateRepository;
+            _voterRepository = voterRepository;
+            _electionRepository = electionRepository;
         }
+
+
+        
     }
 }
