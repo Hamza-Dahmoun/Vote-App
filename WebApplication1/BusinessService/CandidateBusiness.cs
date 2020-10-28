5da5ecd4989740208c3248f6ebf3a576f77d9e77
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-28 08:22:26 +0100

    add GetCandidate_byVoter_byElection() to CandidateBusines.cs

--- WebApplication1/BusinessService/CandidateBusiness.cs
+++ WebApplication1/BusinessService/CandidateBusiness.cs
@@ -125,5 +125,25 @@ namespace WebApplication1.BusinessService
                 throw E;
             }
         }
+
+
+
+        public Candidate GetCandidate_byVoter_byElection(Voter voter, Election election)
+        {
+            //this method gets a candidate by its voterId and its ElectionId
+
+            try
+            {
+                //declaring an expression that is special to Candidate objects and it compares the election instance of the candidates 
+                //with 'election' parameter and voterBeing with voter parameter
+                //System.Linq.Expressions.Expression<Func<Candidate, bool>> expr = i => i.Election == election && i.VoterBeing == voter;
+                System.Linq.Expressions.Expression<Func<Candidate, bool>> expr = i => i.Election == election && i.VoterBeingId == voter.Id;
+                return _candidateRepository.GetOneFiltered(expr);
+            }
+            catch (Exception E)
+            {
+                throw E;
+            }
+        }
     }
 }
