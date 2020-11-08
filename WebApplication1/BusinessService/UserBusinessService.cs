252003981aea9d70cb4d6bc6cb01a30b151a4ba4
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-08 15:12:30 +0100

    create method userBusiness.AddNewUser()

--- WebApplication1/BusinessService/UserBusinessService.cs
+++ WebApplication1/BusinessService/UserBusinessService.cs
@@ -18,5 +18,16 @@ namespace WebApplication1.BusinessService
             _userManager = userManager;
             _contextAccessor = contextAccessor;
         }
+
+        public async Task<IdentityResult> AddNewUser(string userName)
+        {
+            var user = new IdentityUser { UserName = userName };
+
+            //CreateAsync() is an asynchronous method, we have to mark this method with 'async task'
+            //_logger.LogInformation("Storing the new IdentityUser instance in IdentityDB");
+            var result = await _userManager.CreateAsync(user, "Pa$$w0rd");//this password will be automatically hashed
+
+            return result;
+        }
     }
 }
