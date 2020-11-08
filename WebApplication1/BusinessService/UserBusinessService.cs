61132526b2ff4bf7b10af5eeb0aa91624991336c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-08 15:19:05 +0100

    add PreVoter role to new users

--- WebApplication1/BusinessService/UserBusinessService.cs
+++ WebApplication1/BusinessService/UserBusinessService.cs
@@ -21,12 +21,18 @@ namespace WebApplication1.BusinessService
 
         public async Task<IdentityResult> AddNewUser(string userName)
         {
+            //This is user is going to have a 'PreVoter' Role
             var user = new IdentityUser { UserName = userName };
 
             //CreateAsync() is an asynchronous method, we have to mark this method with 'async task'
             //_logger.LogInformation("Storing the new IdentityUser instance in IdentityDB");
             var result = await _userManager.CreateAsync(user, "Pa$$w0rd");//this password will be automatically hashed
-
+            if (result.Succeeded)
+            {
+                //so the user has been added successfully, lets assign him a PreVoter role
+                var result1 = await _userManager.AddToRoleAsync(user, "PreVoter");
+                return result1;
+            }
             return result;
         }
     }
